5481ac8cec825903f50ce13218960faf8c5d241a
vladyslavlopatin <ilopatin3@gmail.com>
2023-09-30 14:42:45 +0200

    removed casts

--- src/Traits/HasCampaign.php
+++ src/Traits/HasCampaign.php
@@ -7,10 +7,6 @@ use Illuminate\Support\Str;
 
 trait HasCampaign
 {
-    protected $casts = [
-        'campaign_unsubscribed_at' => 'datetime',
-    ];
-
     public function campaignEmail(): ?string
     {
         return $this->email && ! $this->campaign_unsubscribed_at ? $this->email : null;
