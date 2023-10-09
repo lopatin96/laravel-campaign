cf3bc8666018226a65d68513b186ce8405e1930f
vladyslavlopatin <ilopatin3@gmail.com>
2023-10-09 21:18:14 +0200

    buildQuery

--- src/Campaigns/Campaign.php
+++ src/Campaigns/Campaign.php
@@ -6,18 +6,47 @@ use Atin\LaravelMail\Models\MailLog;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Mail;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 abstract class Campaign
 {
     protected string $mailable;
 
-    abstract protected function getRecipients(): Collection;
+    protected bool $sendOnlyOnce = true;
+
+    protected bool $doNotSendToUnsubscribed = true;
+
+    protected bool $excludeBlockedUsers = true;
+
+    abstract protected function buildQuery(): Builder;
 
     public function run(): void
     {
-        foreach ($this->getRecipients()->shuffle()->take(config('laravel-campaign.max_emails_per_campaign')) as $user) {
+        foreach ($this->getRecipients() as $user) {
             $this->send($user);
         }
+    }
+
+    private function getRecipients(): Collection
+    {
+        return $this->select('users.*')
+            ->when($this->sendOnlyOnce, function ($query) {
+                $query->leftJoin('mail_logs', function($join) {
+                    $join->on('users.id', '=', 'mail_logs.user_id')
+                        ->where('mail_logs.mail_type', '=', $this->mailable);
+                })
+                    ->whereNull('mail_logs.user_id');
+            })
+            ->when($this->doNotSendToUnsubscribed, function ($query) {
+                $query->whereNull('campaign_unsubscribed_at');
+            })
+            ->when($this->excludeBlockedUsers, function ($query) {
+                $query->whereNotIn('users.status', ['blocked']);
+            })
+            ->shuffle()
+            ->take(config('laravel-campaign.max_emails_per_campaign'))
+            ->distinct()
+            ->get();
     }
 
     private function send(User $user): void
